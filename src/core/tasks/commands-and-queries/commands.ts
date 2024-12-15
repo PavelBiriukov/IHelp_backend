326b882beb42db8faa4f1943023d761007b496d7
AlexeyKrutyakov <coder.arctik@gmail.com>
2024-12-15 18:23:30 +0300

    feat: add command and handler for releasing task by volunteer

--- src/core/tasks/commands-and-queries/commands.ts
+++ src/core/tasks/commands-and-queries/commands.ts
@@ -1,4 +1,9 @@
 import { UpdateVolunteerProfileHandler } from '../../update-volunteer-profile.handler';
 import { CreateTaskChatHandler } from '../../create-chat.handler';
+import { ReleaseTaskVolunteerHandler } from '../../release-task-volunteer.handler';
 
-export const COMMANDS = [UpdateVolunteerProfileHandler, CreateTaskChatHandler];
+export const COMMANDS = [
+  UpdateVolunteerProfileHandler,
+  CreateTaskChatHandler,
+  ReleaseTaskVolunteerHandler,
+];
