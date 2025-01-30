d188d54948eaa9f838ca39b1513ad8c6510da416
Evgeny Karpel <kspshnik@yandex.ru>
2025-01-30 23:48:11 +0300

    [Hotfix] Added correct treatment of `lastread === null | undefined`.

--- src/common/helpers/is-message-earlier.ts
+++ src/common/helpers/is-message-earlier.ts
@@ -1,5 +1,6 @@
 import { MessageInterface } from '../types/chats.types';
+import { ensureDate } from './ensure-date';
 
 export function isMessageEarlier(msg: MessageInterface, timestamp: Date): boolean {
-  return msg.timestamp.getTime() > timestamp.getTime();
+  return ensureDate(msg.timestamp).getTime() > timestamp.getTime();
 }
