a1935739c4ca5d4ea7d8ebb60fa525971efeb980
Evgeny Karpel <kspshnik@yandex.ru>
2025-01-30 22:32:03 +0300

    [Hotfix] Fixed Reference error on empty messages array in `makeUnreadsMeta()`

--- src/common/helpers/make-unreads-meta.ts
+++ src/common/helpers/make-unreads-meta.ts
@@ -2,5 +2,13 @@ import { MessageInterface } from '../types/chats.types';
 
 export function makeUnreadsMeta(unreads: Array<MessageInterface>): [number, string] {
   const [watermark] = unreads;
-  return [unreads.length, watermark.timestamp.toISOString()];
+  if (!watermark) {
+    return [0, null];
+  }
+  return [
+    unreads.length,
+    typeof watermark.timestamp === 'string'
+      ? watermark.timestamp
+      : watermark?.timestamp?.toISOString(),
+  ];
 }
