09a1e1366ee608aa17aed4f3da31e8ea6829aa8c
Evgeny Karpel <kspshnik@yandex.ru>
2025-01-30 23:46:58 +0300

    [Hotfix] Added correct treatment of `lastread === null | undefined`.

--- src/common/helpers/get-unread-messages.ts
+++ src/common/helpers/get-unread-messages.ts
@@ -1,11 +1,17 @@
 import { MessageInterface } from '../types/chats.types';
 import { compareDates } from './compare-dates';
+import { ensureDate } from './ensure-date';
 
 export function getUnreadMessages(
   messages: Array<MessageInterface>,
   lastread: Date
 ): Array<MessageInterface> {
-  const unreads = messages.filter((msg) => msg.timestamp.getTime() > lastread.getTime());
-  unreads.sort((a, b) => compareDates(a.timestamp, b.timestamp));
+  if (!lastread || messages.length === 0) {
+    return [];
+  }
+  const unreads = messages.filter(
+    (msg) => ensureDate(msg.timestamp).getTime() > lastread.getTime()
+  );
+  unreads.sort((a, b) => compareDates(ensureDate(a.timestamp), ensureDate(b.timestamp)));
   return unreads;
 }
