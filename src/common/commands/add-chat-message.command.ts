f03325f8f4d71f76e8e3e1e960063896b961bf13
Evgeny Karpel <kspshnik@yandex.ru>
2025-02-03 22:33:39 +0300

    [Refactor] User passed down from gateway.

--- src/common/commands/add-chat-message.command.ts
+++ src/common/commands/add-chat-message.command.ts
@@ -1,5 +1,6 @@
 import { WsNewMessage } from '../types/websockets.types';
+import { AnyUserInterface } from '../types/user.types';
 
 export class AddChatMessageCommand {
-  constructor(public readonly message: WsNewMessage) {}
+  constructor(public readonly message: WsNewMessage, public readonly user: AnyUserInterface) {}
 }
